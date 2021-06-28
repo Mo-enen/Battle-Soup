45fb99078f00be574433d5ce6e905a31f0cef41c
Moenenn <moenen6@gmail.com>
2021-06-28 22:03:39 +0800

    0628

---
+++ Assets/Battle Soup/Soup.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+namespace BattleSoup {
+
+
+
+
+	public struct Tile {
+		public bool Revealed;
+		public bool HasStone;
+		
+	}
+
+
+	public struct Ship {
+
+
+
+	}
+
+
+	public static class Soup {
+
+
+		public static void Analyse (Tile[,] tiles, Ship[] myShips) {
+
+
+
+		}
+
+
+	}
+}
